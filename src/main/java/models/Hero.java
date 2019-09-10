587251cca90791ea7bc4228d47b87ae74c415318
barakamulas <barakafrank18@gmail.com>
2019-09-10 10:36:11 +0300

    Define class Hero

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,71 @@
 package models;
 
 public class Hero {
+
+
+    private int id;
+    private String name;
+    private int age;
+    private String specialPower;
+    private String weakness;
+    private int squadId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public void setSpecialPower(String specialPower) {
+        this.specialPower = specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
+
+    public int getSquadId() {
+        return squadId;
+    }
+
+    public void setSquadId(int squadId) {
+        this.squadId = squadId;
+    }
+
+    public Hero(String name, int age, String specialPower, String weakness, int squadId) {
+        this.name = name;
+        this.age = age;
+        this.specialPower = specialPower;
+        this.weakness = weakness;
+        this.squadId = squadId;
+    }
 }
+
+
+
