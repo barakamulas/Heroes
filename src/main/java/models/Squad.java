276e8a0daa05d30e3653f269d8437afffa0c847f
barakamulas <barakafrank18@gmail.com>
2019-09-10 10:24:42 +0300

    Define class Squad

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,4 +1,54 @@
 package models;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Squad {
+
+    private String name;
+    private String cause;
+    private int maxSize;
+    private int id;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Squad(String name, String cause, int maxSize) {
+        this.name = name;
+        this.cause = cause;
+        this.maxSize = maxSize;
+    }
+
+
 }
